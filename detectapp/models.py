54b1254c4276672cc304e4c719685af014a5d49e
Gokulahari <gokulahari1234@gmail.com>
2019-10-24 19:18:20 +0530

    Initialize Django project

---
+++ detectapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, RegexValidator
+
+
+class Student(models.Model):
+    pro_name = models.CharField(max_length=100, null=True)
+    # age = models.IntegerField(max_length=100, null=True)
+    pro_price = models.CharField(primary_key=False, max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
+
+    # address = models.CharField(max_length=200, blank=True)
+    # def __str__(self):
+    #     return self.age
---
+++ detectapp/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import HttpResponse,render
+from .models import Student
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate(
+        'C:\\Users\\Gokulahari\\Desktop\\firebase stuffs\\Grocery project\\python firebase\\firebase-admin-sdk.json')
+firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://grocery-7a56e.firebaseio.com'
+    })
+def home(request):
+    if request.method == 'POST':
+        if (request.POST.get('click')):
+            p_name = request.POST.get('nam')
+            # p_id = request.POST.get('ii')
+            p_price = request.POST.get('rs')
+            if p_name and p_price:
+                ins = Student(pro_name=p_name, pro_price=p_price)
+                ins.save()
+                return render(request, 'web/main.html',{'k':"Added Successfully"})
+            else:
+                return render(request, 'web/main.html', {'k': "Fill the product name and price"})
+            # return HttpResponse(fol)
+
+        elif(request.POST.get('click1')):
+            p_name = request.POST.get('nam')
+            if p_name:
+                Student.objects.filter(pro_name=p_name).delete()
+                return render(request, 'web/main.html', {'k': "Deleted Successfully"})
+            else:
+                return render(request, 'web/main.html', {'k': "Enter the product Name to delete"})
+
+        elif (request.POST.get('click2')):
+            data = {}
+            p = Student.objects.all()
+            data["prod"] = p
+            return render(request, 'web/product.html',data)
+
+        elif (request.POST.get('click4')):
+            ref = db.reference('Orders')
+            ans = ref.get()
+
+            return render(request, 'web/orders.html',{'k':ans})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    data = Student.objects.all()
+    # return HttpResponse(data)
+    # # for i in data:
+    # #     l.append(i.name)
+    return render(request,"web/main.html",{"w":data})
+
